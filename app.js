cef2b63d927b728ae2378351966f93596ed0f8c3
harsh saxena <kinshuk26harsh@gmail.com>
2022-11-06 01:52:15 +0530

    now made the working to do list

--- app.js
+++ app.js
@@ -4,49 +4,31 @@ const https = require("https");
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended:true}));
+var items =[];
 app.get("/", function (req,res)
 {
     // res.send("this is  a to do list logic");
     var today= new Date();
-    var currentDay = today.getDay();
-     var day="";
-    
-     switch(currentDay)
-     {
-        case 0:
-            day="sunday";
-            break;
-            case 1:
-                day="Monday";
-                break;
-                case 2:
-                    day="Tuesday";
-                    break;
-                    case 3:
-                        day="Wednesday";
-                        break;
-                        case 4:
-                            day="thursday";
-                            break;
-                            case 5:
-                                day="friday";
-                                break;
-                                case 6:
-                                    day="saturday";
-                                    break;
-                                    default:
-                                        console.log("error");
-      }
-    // if (currentDay==6|| currentDay==0)
-    // {
-    //     day="weekend";
-    // }
-    // else{
-    //     day="weekday";
-    // }
-    res.render("list",{kindofDay :day});
+
+var options ={
+    weekday: "long",
+    day:"numeric",
+    month:"long"
+};
+var day = today.toLocaleDateString("en-US",options);
+
+    res.render("list",{kindofDay :day ,newListItems :items});
+
 });
+
+app.post("/" , function(req,res)
+{
+      var item = req.body.newItem; 
+    items.push(item);
+    res.redirect("/");
+});
+
 app.listen(7000,function()
 {
     console.log("server working at port 7000");
